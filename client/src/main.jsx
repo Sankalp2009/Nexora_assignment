23d63eb6aa29e7165e14bc36337667019e2dbcec
Sankalp Patel <101709238+Sankalp2009@users.noreply.github.com>
2025-11-13 17:00:43 +0530

    Package.json_Modified

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,12 @@ import App from "./App.jsx";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000, // default is 500
   },
-  base: "/Nexora_assignment/",
+  base: "/Nexora_assignment",
 });
